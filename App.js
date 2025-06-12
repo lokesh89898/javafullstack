d828d973a0f844e5a85c94440d5520c861250004
rizwana2004 <lokeshpolagani04@gmail.com>
2025-06-12 14:48:05 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import logo from './logo sviet.png';
+import './App.css';
+import './intro';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo sviet" />
+        <p>
+          <code>Sviet Nandhamuru</code>
+        </p>
+        <a
+          className="App-link"
+          href="http://103.208.229.211/newecap/default.aspx"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          Sri vasavi institution of engeneering and technology login
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
